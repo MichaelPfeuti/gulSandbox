c556be3a3cec1454eaac1b9dea4bdd9507f6d1a7
Michael Pfeuti <mpfeuti@ganymede.ch>
2013-05-17 09:53:18 +0200

    first tutorial

--- src/videoPlayer/main.cpp
+++ src/videoPlayer/main.cpp
@@ -1,23 +1,78 @@
+#include <GL/glew.h>
 #include <GL/glfw.h>
+#include <cstdio>
 
 int main(void)
 {
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    if (!glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW))
-        return -1;
+  if(!glfwInit())
+    return -1;
 
-    /* Loop until the user closes the window */
-    while (glfwGetWindowParam(GLFW_OPENED))
-    {
-        /* Render here */
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        /* Swap front and back buffers and process events */
-        glfwSwapBuffers();
-    }
+  /* Create a windowed mode window and its OpenGL context */
+  if (!glfwOpenWindow( 1024, 768, 0,0,0,0, 32,0, GLFW_WINDOW ))
+  {
+    fprintf(stderr, "Failed to open GLFW window\n");
+    glfwTerminate();
+    return -1;
+  }
 
-    return 0;
+  glewExperimental=true; // Needed in core profile
+  if (glewInit() != GLEW_OK) {
+      fprintf(stderr, "Failed to initialize GLEW\n");
+      return -1;
+  }
+
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+
+  // An array of 3 vectors which represents 3 vertices
+  static const GLfloat g_vertex_buffer_data[] = {
+     -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     0.0f,  1.0f, 0.0f,
+  };
+
+  // This will identify our vertex buffer
+  GLuint vertexbuffer;
+
+  // Generate 1 buffer, put the resulting identifier in vertexbuffer
+  glGenBuffers(1, &vertexbuffer);
+
+  // The following commands will talk about our 'vertexbuffer' buffer
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+  // Give our vertices to OpenGL.
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+  bool isInit = false;
+  /* Loop until the user closes the window */
+  while (glfwGetWindowParam(GLFW_OPENED))
+  {
+      // 1rst attribute buffer : vertices
+      glEnableVertexAttribArray(0);
+      glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+      glVertexAttribPointer(
+         0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+         3,                  // size
+         GL_FLOAT,           // type
+         GL_FALSE,           // normalized?
+         0,                  // stride
+         (void*)0            // array buffer offset
+      );
+
+      // Draw the triangle !
+      glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+      glDisableVertexAttribArray(0);
+
+    /* Swap front and back buffers and process events */
+    //glfwSwapBuffers();
+  }
+
+  return 0;
 }
